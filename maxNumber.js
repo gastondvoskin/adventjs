b3ba46186af3488e524bf143b802df3d04ca0cf6
Gaston Dvoskin <dvoskingaston@gmail.com>
2024-08-04 16:50:50 -0300

    Solved maxNumber.js

---
+++ maxNumber.js
@@ -0,0 +1,12 @@
+const maxNumber = (array) => { 
+  return Math.max(...array);
+}; 
+
+// TESTING
+// console.log(maxNumber([1, -1, 7, 2, -5]));  // 7
+// console.log(maxNumber([-3, -1, -5])); // -1
+
+
+// Exercise
+// 11. Create a function that finds the maximum number in an array
+// In this challenge, you’ll write a function that takes an array of numbers as an input and returns the largest number in that array. Test your function with various arrays, including arrays with negative numbers, decimals, and all positive integers.
