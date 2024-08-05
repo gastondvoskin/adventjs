45f56f28c0707f1777c09af1382b120940cabc33
Gaston Dvoskin <dvoskingaston@gmail.com>
2024-08-05 19:07:47 -0300

    reverseString second solution

--- 4.reverseString.js
+++ 4.reverseString.js
@@ -1,5 +1,16 @@
+// Solution 1
+// const reverseString = (string) => {
+//   const reversedString = string.split("").reverse().join(""); 
+//   return reversedString; 
+// }; 
+
+// Solution 2
 const reverseString = (string) => {
-  const reversedString = string.split("").reverse().join(""); 
+  let reversedString = ""; 
+  for (let i = string.length - 1; i >= 0; i--) {
+    const char = string[i];
+    reversedString += char;
+  };
   return reversedString; 
 }; 
 
