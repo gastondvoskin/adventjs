17a9a3a79f300b2d1612165f731b2735d9353e6b
Gaston Dvoskin <dvoskingaston@gmail.com>
2024-08-18 16:45:04 -0300

    Solved 2021-07.js without recursion

--- adventjs/2021-07b.js
+++ adventjs/2021-07b.js
@@ -1,11 +1,9 @@
-const saveStringsOfObject = (object) => {
+export default function contains(store, product) {
   const strings = [];
-  const stack = [object];
-  console.log(stack.length)
+  const stack = [store];
 
   while (stack.length > 0) {
     const current = stack.pop();
-    console.log(current)
     for (const key in current) {
       if (Object.prototype.hasOwnProperty.call(current, key)) {
         const element = current[key];
@@ -16,7 +14,35 @@ const saveStringsOfObject = (object) => {
         }
 
         // if the element is an object and not null, push it onto the stack
-        if (typeof element === 'object' && element !== null) {
+        if (typeof element === "object" && element !== null) {
+          stack.push(element);
+        }
+      }
+    }
+  }
+
+  return strings.includes(product);
+}
+
+const saveStringsOfObject = (object) => {
+  const strings = [];
+  const stack = [object];
+  console.log(stack.length);
+
+  while (stack.length > 0) {
+    const current = stack.pop();
+    console.log(current);
+    for (const key in current) {
+      if (Object.prototype.hasOwnProperty.call(current, key)) {
+        const element = current[key];
+
+        // base case
+        if (typeof element === "string") {
+          strings.push(element);
+        }
+
+        // if the element is an object and not null, push it onto the stack
+        if (typeof element === "object" && element !== null) {
           stack.push(element);
         }
       }
@@ -24,7 +50,7 @@ const saveStringsOfObject = (object) => {
   }
 
   return strings;
-}
+};
 
 const object1 = {
   key1: "value1",
@@ -32,10 +58,10 @@ const object1 = {
     subkey1: "value2",
     subkey2: {
       subsubkey1: "value3",
-      subsubkey2: 42
-    }
+      subsubkey2: 42,
+    },
   },
-  key3: "value4"
+  key3: "value4",
 };
 
 console.log(saveStringsOfObject(object1)); // ["value1", "value2", "value3", "value4"]
