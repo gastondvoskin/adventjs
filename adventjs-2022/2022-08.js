9b6ebe625200db204dfe9064e621a04e4131042a
Gaston Dvoskin <dvoskingaston@gmail.com>
2024-09-26 12:14:18 -0300

    Implemented isPalindrome helper function

--- adventjs-2022/2022-08.js
+++ adventjs-2022/2022-08.js
@@ -2,13 +2,37 @@
 //   return false
 // }
 
+const isPalindrome = (part) => {
+  const partArr = part.split("");
+  for (let i = 0; i < partArr.length / 2; i++) {
+    const char_A = partArr[i];
+    const char_B = partArr[partArr.length - 1 - i];
+    if (char_A !== char_B) return false; 
+  }
+  return true;
+}
+
+// TESTING isPalindrome
+console.log(isPalindrome("abc")); 
+console.log(isPalindrome("aba")); 
+
+
 function checkPart(part) {
   return false
 }
 
 
+// TESTING
+checkPart("a") // true
 
-
+// LOGIC
+/* 
+if part is palindrome return true
+if part removing part[0] is palindrome return true
+if part removing part[2] is palindrome return true
+... 
+return false
+*/
 
 // EXERCISE
 // Some electric sleds have broken down and the elves are looking for spare parts to fix them, but they are not sure if the parts they have are valid.
