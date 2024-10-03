d6788845529e36a3c12dce67bf8fad0c0f3f9d42
Gaston Dvoskin <dvoskingaston@gmail.com>
2024-10-03 10:57:19 -0300

    Solved 2022-10.js

--- adventjs-2022/2022-10.js
+++ adventjs-2022/2022-10.js
@@ -2,15 +2,83 @@
 //   return false
 // }
 
+// function checkJump(heights) {
+//   if (heights.length < 3) return false; 
+//   const maxValue = Math.max(...heights);
+//   const indexOfMaxValue = heights.findIndex((height) => height === maxValue); 
+//   console.log(maxValue, indexOfMaxValue)
+
+//   let wentUp = false; 
+//   let wentDown = false;  
+
+//   for (let i = 0; i < heights.length - 1; i++) {
+//     const currentHeight = heights[i];
+//     const nextHeight = heights[i + 1];
+//     console.log(i, currentHeight, nextHeight); 
+//     if (i < indexOfMaxValue && currentHeight > nextHeight) {
+//       return false;
+//     }; 
+//     if (i >= indexOfMaxValue && currentHeight < nextHeight) {
+//       return false;
+//     }
+//   }
+//   return wentUp && wentDown; 
+// }
+
+function checkJump(heights) {
+  let direction = "initial"; /* initial up down */
+  let changedToUp = 0;  
+  let changedToDown = 0;  
+
+  for (let i = 0; i < heights.length - 1; i++) {
+    const currHeight = heights[i];
+    const nextHeight = heights[i + 1];
+    console.log(i, currHeight, nextHeight); 
+
+    if (currHeight < nextHeight && (direction === "initial" || direction === "down")) {
+      console.log(i); 
+      changedToUp++; 
+      direction = "up";       
+    } 
+    else if (currHeight > nextHeight) {
+      if (direction === "initial") {
+        console.log(i); 
+        return false; 
+      } 
+      else if (direction === "up") {
+        console.log(i); 
+        changedToDown++; 
+        direction = "down";         
+      }  
+    }
+    if (changedToUp > 1 || changedToDown > 1) {
+      console.log(i); 
+      return false; 
+    }
+  }
+  return changedToUp === 1 && changedToDown === 1;  
+}
+
 
 
 // TESTING
-console.log(checkJump([])) // false
-console.log(checkJump([1])) // false
-console.log(checkJump([1, 1])) // false
-console.log(checkJump([1, 1, 1])) // false
-console.log(checkJump([1, 2, 1])) // true
+console.log(checkJump([1, 3, 8, 5, 2])) // true
+console.log(checkJump([1, 7, 3, 5])) // false
 
+// console.log(checkJump([])) // false
+// console.log(checkJump([1])) // false
+// console.log(checkJump([1, 1])) // false
+// console.log(checkJump([1, 1, 1])) // false
+// console.log(checkJump([1, 2, 1])) // true
+// console.log(checkJump([1, 3, 6, 4, 5, 1])) // false
+
+
+// LOGIC
+// check if the first movement os up. then
+// The jump is valid if it goes up once and down once. If the sleigh stays at the same height between two positions, the parabola continues.
+// let direction = "up"; 
+// let directionChanges = 0;
+// find the maxHeight. check previos values. check next values.  
 
 
 // EXERCISE
