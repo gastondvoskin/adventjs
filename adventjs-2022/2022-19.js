5ad3e1ef471c9da4fcae541cad865c10866005db
Gaston Dvoskin <dvoskingaston@gmail.com>
2024-11-28 10:58:19 -0300

    Commented tests

--- adventjs-2022/2022-19.js
+++ adventjs-2022/2022-19.js
@@ -15,16 +15,16 @@ function sortToys(toys, positions) {
 }
 
 // TEST
-sortToys(["b", "a"], [2,1]) // ["a", "b"] 
+// sortToys(["b", "a"], [2,1]) // ["a", "b"] 
 
-const toys = ['ball', 'doll', 'car', 'puzzle']
-const positions = [2, 3, 1, 0]
-console.log(sortToys(toys, positions))
-// ['puzzle', 'car', 'ball', 'doll']
+// const toys = ['ball', 'doll', 'car', 'puzzle']
+// const positions = [2, 3, 1, 0]
+// console.log(sortToys(toys, positions))
+// // ['puzzle', 'car', 'ball', 'doll']
 
-const moreToys = ['pc', 'xbox', 'ps4', 'switch', 'nintendo']
-const morePositions = [8, 6, 5, 7, 9]
-console.log(sortToys(moreToys, morePositions))
+// const moreToys = ['pc', 'xbox', 'ps4', 'switch', 'nintendo']
+// const morePositions = [8, 6, 5, 7, 9]
+// console.log(sortToys(moreToys, morePositions))
 // ['ps4', 'xbox', 'switch', 'pc', 'nintendo']
 
 
