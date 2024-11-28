fc56b5b51699f626e12b9cfb9be80561ca270e6b
Gaston Dvoskin <dvoskingaston@gmail.com>
2024-11-28 10:36:49 -0300

    Solved 2022-19.js

--- adventjs-2022/2022-19.js
+++ adventjs-2022/2022-19.js
@@ -3,10 +3,47 @@
 // }
 
 function sortToys(toys, positions) {
-  return []
+  let toysWithPositions = []; 
+  for (let i = 0; i < toys.length; i++) {
+    const toy = toys[i];
+    const position = positions[i];
+    const toyWithPosition = {toy, position}; 
+    toysWithPositions.push(toyWithPosition); 
+    // console.log( toy, position, toyWithPosition); 
+  }
+  console.log(toysWithPositions); 
+
+  toysWithPositions.sort((a, b) => a.position - b.position); 
+  console.log(toysWithPositions); 
+
+  const sortedToys = toysWithPositions.map((element) => element.toy); 
+  console.log(sortedToys); 
+  return sortedToys; 
 }
 
+// TEST
+// sortToys(["b", "a"], [2,1]) // ["a", "b"] 
 
+// const toys = ['ball', 'doll', 'car', 'puzzle']
+// const positions = [2, 3, 1, 0]
+// console.log(sortToys(toys, positions))
+// ['puzzle', 'car', 'ball', 'doll']
+
+// const moreToys = ['pc', 'xbox', 'ps4', 'switch', 'nintendo']
+// const morePositions = [8, 6, 5, 7, 9]
+// console.log(sortToys(moreToys, morePositions))
+// ['ps4', 'xbox', 'switch', 'pc', 'nintendo']
+
+
+
+
+/* 
+LOGIC 
+goal: sort the toys acoording to the positions. 
+["b", "a"], [1,0] ---> arrOfObj [{toy: "b", position: 2}, {toy: "a", position: 1}]
+arrOfObj.sort((a,b) => a.position - b.position)
+finalArr = arrOfObj.map(obj => obj.toy)
+*/
 
 // EXERCISE
 // The day is coming and Santa Claus has the toy store a mess. Help him to sort the toys in the warehouse so he can find them easier.
