1191a3f742b562b190a6c53bd6e552f4eab43f1c
Gaston Dvoskin <dvoskingaston@gmail.com>
2024-12-05 22:00:00 -0300

    Solved 05.js

--- adventjs-2024/05.js
+++ adventjs-2024/05.js
@@ -3,19 +3,51 @@
 // }
 
 function organizeShoes(shoes) {
-  return []
-}
+  let finalArray = []; 
 
+  for (let i = 0; i < shoes.length - 1; i++) {
+    const shoeOne = shoes[i];
+    for (let j = i + 1; j < shoes.length; j++) {
+      console.log(i, j)
+      const shoeTwo = shoes[j];
+      console.log(shoeOne); 
+      console.log(shoeTwo); 
+      if (shoeOne.size === shoeTwo.size && shoeOne.type !== shoeTwo.type) {
+        console.log("here"); 
+        finalArray.push(shoeOne.size); 
+        // delete shoeTwo to avoid duplicating it
+        shoes.splice(j, 1); 
+        console.log(shoes); 
+        break;
+      }
+    }
+  }
+
+  console.log(finalArray); 
+
+  return finalArray; 
+}
 
 // TEST
 organizeShoes([
-  { type: 'I', size: 38 },
+  { type: 'I', size: 42 },
   { type: 'R', size: 42 },
   { type: 'R', size: 38 },
+  { type: 'R', size: 42 },
+  { type: 'I', size: 42 },
 ])
 
 /* 
 LOGIC
+NEW
+let finalArray = []; 
+iterate shoes
+  iterate inside from the second element
+  compare shoeOne to ShoeTwo. if size is equal && type is not equal ---> finalArray.push(size)
+
+return finalArray
+
+
 OLD
 let finalArray = []; 
 let orderedShoes = [];
@@ -25,13 +57,6 @@ iterate shoes
     if currentShoe.size === orderedShoe.size && orderedShoe.type !== orderedShoe.size ---> {1. finalArray.push(currentShoe.size)  2. orderedShoes[j] splice }
   at the end of the second loop ---> orderedShoes.push(currentShoe)
 
-NEW
-let finalArray = []; 
-iterate shoes
-  iterate inside from the second element
-  compare shoeOne to ShoeTwo. if size is equal && type is not equal ---> finalArray.push(size)
-
-return finalArray
 
 */
 
