6a689c1527525ec2da077bc764e25719838fbbbc
Gaston Dvoskin <dvoskingaston@gmail.com>
2024-12-11 13:29:34 -0300

    Solved adventjs-2024 10.js. Check online tests

--- adventjs-2024/10.js
+++ adventjs-2024/10.js
@@ -4,9 +4,67 @@
 // }
 
 function compile(instructions) {
-  // Code here
-  return 0
+  const MOV = "MOV";
+  const INC = "INC";
+  const DEC = "DEC";
+  const JMP = "JMP";
+  
+  let values = {}; 
+  
+  for (let i = 0; i < instructions.length; i++) {
+    const instruction = instructions[i];
+    const [action, x, y] = instruction.split(" "); 
+    
+    if (action === MOV) {
+      const xIsNumber = !isNaN(Number(x)); 
+      values[y] = xIsNumber ? Number(x) : Number(values[x]); 
+    }
+    else if (action === INC) {
+      values[x] = !values[x] ? 0 + 1 : values[x] + 1;
+    }
+    else if (action === DEC) {
+      values[x] = !values[x] ? 0 - 1 : values[x] - 1;
+    }
+    else if (action === JMP && values[x] === 0) {
+      i = y - 1;
+    }
+    console.log(values)
+  }
+
+  return values.A; 
 }
+
+
+// TEST 
+// console.log(compile(["MOV -1 C", "INC C", "DEC C", "JMP C 0" ]));  
+// console.log(compile(["JMP C 0" ]));  
+// console.log(compile(["INC A" ])); 
+console.log(compile(["MOV 0 A", "INC A"])); 
+console.log(compile(["INC A", "INC A", "DEC A", "MOV A B"])); 
+console.log(compile(["MOV 5 B", "DEC B", "MOV B A", "INC A"])); 
+console.log(compile(["MOV 2 X", "DEC X", "JMP X 1", "MOV X A"])); 
+
+
+// const instructions = [
+//   'MOV -1 C', // copies -1 to register 'C',
+//   'INC C', // increments the value of register 'C'
+//   'JMP C 1', // jumps to the instruction at index 1 if 'C' is 0
+//   'MOV C A', // copies register 'C' to register 'A',
+//   'INC A', // increments the value of register 'A'
+// ]
+
+// console.log(compile(instructions)); // -> 2
+
+//  Step-by-step execution:
+//  0: MOV -1 C -> The register C receives the value -1
+//  1: INC C    -> The register C becomes 0
+//  2: JMP C 1  -> C is 0, jumps to the instruction at index 1
+//  1: INC C    -> The register C becomes 1
+//  2: JMP C 1  -> C is 1, the instruction is ignored
+//  3: MOV C A  -> Copies register C to A. Now A is 1
+//  4: INC A    -> The register A becomes 2
+
+
 
 
 /* 
