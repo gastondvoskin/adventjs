acd95fe61464a2111d8f3dcc871ea049cc568650
Gaston Dvoskin <dvoskingaston@gmail.com>
2024-12-12 18:24:48 -0300

    Solved adventjs-2024 12.js

--- adventjs-2024/12.js
+++ adventjs-2024/12.js
@@ -4,9 +4,68 @@
 // }
 
 function calculatePrice(ornaments) {
-  // Code here
-  return 0      
+  const values = {
+    "*": 1,
+    "o": 5,
+    "^": 10,
+    "#": 50,
+    "@": 100,
+  }; 
+  
+  const valuesKeys = Object.keys(values);
+  console.log(valuesKeys); 
+
+  let price = 0; 
+  const ornamentsArray = ornaments.split("");
+  console.log(ornamentsArray); 
+
+
+  for (let i = 0; i < ornamentsArray.length; i++) {
+    const ornament = ornamentsArray[i];
+    if (!valuesKeys.includes(ornament)) {
+      return undefined;
+    }    
+  }; 
+
+  for (let i = 0; i < ornamentsArray.length; i++) {
+    const ornament = ornamentsArray[i];
+    console.log(ornament); 
+    const nextOrnament = ornamentsArray[i + 1];
+    console.log(nextOrnament); 
+
+    let ornamentValue = values[ornament]; 
+    console.log(ornamentValue); 
+    const nextOrnamentValue = values[nextOrnament]; 
+    console.log(nextOrnamentValue); 
+
+    if (ornamentValue < nextOrnamentValue) {
+      ornamentValue = -ornamentValue;
+      console.log(ornamentValue);
+    }
+
+    price += ornamentValue; 
+    console.log(price); 
+  }
+
+  console.log(price); 
+  return price;  
 }
+
+// TEST 
+// console.log(calculatePrice('*o#'));   
+// console.log(calculatePrice('*bo'));   
+
+// calculatePrice('***')  // 3   (1 + 1 + 1)
+// calculatePrice('*o')   // 4   (5 - 1)
+// calculatePrice('o*')   // 6   (5 + 1)
+// calculatePrice('*o*')  // 5  (-1 + 5 + 1) 
+// calculatePrice('**o*') // 6  (1 - 1 + 5 + 1) 
+// calculatePrice('o***') // 8   (5 + 3)
+// calculatePrice('*o@')  // 94  (-5 - 1 + 100)
+// calculatePrice('*#')   // 49  (-1 + 50)
+// calculatePrice('@@@')  // 300 (100 + 100 + 100)
+// calculatePrice('#@')   // 50  (-50 + 100)
+// calculatePrice('#@Z')  // undefined (Z is unknown) 
 
 
 
