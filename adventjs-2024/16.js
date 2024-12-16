c226f23f389c3ecc0651bc29cbaa628e61254e85
Gaston Dvoskin <dvoskingaston@gmail.com>
2024-12-16 15:27:10 -0300

    Solved 16.js

--- adventjs-2024/16.js
+++ adventjs-2024/16.js
@@ -4,9 +4,54 @@
 // }
 
 function removeSnow(s) {
-  // Code here
-  return '';
+  let snowArray = s.split(""); 
+  console.log(snowArray);
+  
+  for (let i = 0; i < snowArray.length - 1; i++) {
+    console.log(i)
+    const currentPile = snowArray[i];
+    const nextPile = snowArray[i + 1];
+    console.log(currentPile); 
+    console.log(nextPile); 
+    console.log(i, currentPile === nextPile); 
+
+    if (currentPile === nextPile) {
+      console.log(i); 
+      snowArray.splice(i, 2); 
+      console.log(snowArray); 
+      i = i - 2;
+    }
+  }
+  console.log(snowArray); 
+  const finalPath = snowArray.join(""); 
+  console.log(finalPath); 
+  return finalPath; 
 }
+
+// TEST 
+// console.log(removeSnow("abccb"))
+//          abb
+//          01234
+
+console.log(removeSnow('zxxzoz')) // -> "oz"
+// 1. Remove "xx", resulting in "zzoz"
+// 2. Remove "zz", resulting in "oz"
+
+console.log(removeSnow('abcdd')) // -> "abc"
+// 1. Remove "dd", resulting in "abc"
+
+console.log(removeSnow('zzz')) // -> "z"
+// 1. Remove "zz", resulting in "z"
+
+console.log(removeSnow('a')) // -> "a"
+// No duplicate piles
+
+/* 
+Logic iterate s. 
+if currentElement === nextElemeng -> remove both elements and i = i - 2;
+*/
+
+
 
 /* 
 EXERCISE
