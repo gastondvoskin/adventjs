be30ae7104ba843fd1ada5a37f55e0d07c0a399c
Gaston Dvoskin <dvoskingaston@gmail.com>
2024-12-21 13:04:39 -0300

    Solved adventjs-2024 21.js

--- adventjs-2024/21.js
+++ adventjs-2024/21.js
@@ -3,10 +3,69 @@
 //   return 0
 // }
 
+
 function treeHeight(tree) {
-  // Write your code here
-  return 0
+  if (tree === null) return 0;
+  
+  const leftHeight = treeHeight(tree.left);
+  const rightHeight = treeHeight(tree.right);
+
+  return Math.max(leftHeight, rightHeight) + 1; 
 }
+
+
+// TEST 
+console.log(treeHeight({
+  value: ".", 
+  left: null, 
+  right: null, 
+}))
+
+console.log(treeHeight({
+  value: ".", 
+  left: {
+    value: ".", 
+    left: null, 
+    right: null, 
+  }, 
+  right: null, 
+}))
+
+console.log(treeHeight({
+  value: ".", 
+  left: {
+    value: ".", 
+    left: null, 
+    right: null, 
+  }, 
+  right: {
+    value: ".", 
+    left: null, 
+    right: null, 
+  },  
+}))
+
+console.log(treeHeight({
+  value: ".", 
+  left: {
+    value: ".", 
+    left: null, 
+    right: null, 
+  }, 
+  right: {
+    value: ".", 
+    left: null, 
+    right: {
+      value: ".", 
+      left: null, 
+      right: {
+        value: ".", 
+        left: null, 
+        right: null, 
+      }, 
+    }, 
+  },  
+}))
 
 
 
