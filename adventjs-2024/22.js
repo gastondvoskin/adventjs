f211cf743ca0955151f476751122823bb9617613
Gaston Dvoskin <dvoskingaston@gmail.com>
2024-12-23 10:48:10 -0300

    Started logic in 22.js (WIP)

--- adventjs-2024/22.js
+++ adventjs-2024/22.js
@@ -4,30 +4,64 @@
 // }
 
 function generateGiftSets(gifts) {
-  // Code here
-  return []
+  let combinations = [];
+  let indexs = gifts.map((gift, index) => index); 
+  console.log(indexs); 
+
+  let i = 0; 
+  while (indexs.length) {
+    const combination = indexs.map(index => gifts[index]); 
+    console.log(combination); 
+    combinations.unshift(combination);
+
+    if (indexs[0] === 0 && indexs[indexs.length - 1] === indexs.length - 1) {
+      console.log("here", indexs);
+      indexs.shift();
+      let newIndex = gifts.length - 1;
+      for (let j = indexs.length - 1; j >= 0; j--) {
+        console.log(j); 
+        indexs.splice(j, 1, newIndex); 
+        newIndex--; 
+      }
+      console.log(indexs); 
+      // overwrite indexs
+    }
+    else if (false) {
+      let indexToReplace = indexs[i];
+      indexs.splice(i, indexToReplace - 1); 
+    }
+
+    indexs = []; /* hardcoded to stop iteration */
+  }
+
+  console.log(combinations); 
+  return combinations;
 }
 
 
 // TESTS 
-generateGiftSets([1, 2, 3]); 
+generateGiftSets(["a", "b", "c"]); 
+// generateGiftSets([1, 2, 3]); 
 /* 
 [
+  [0], 
   [1], 
   [2], 
-  [3], 
-  [4],            
-  [1, 2],   
-  [1, 3], 
-  [1, 4], 
-  [2, 3], 
-  [2, 4], 
-  [3, 4],         
-  [1, 2, 3], 
-  [1, 2, 4], 
-  [1, 3, 4], 
-  [2, 3, 4],      
-  [1, 2, 3, 4], 
+  [3],            
+  [0, 1],         
+  [0, 2],              
+  [0, 3],             // i = 1
+  [1, 2],             // i = 0  -- and j = 1 ++
+  [1, 3],             // i = 1
+  [2, 3],             // i = 0 
+  [0, 1, 2],          // .shift()
+  [0, 1, 3],          // i = 2   --      
+  [0, 2, 3],          // i = 1   -- 
+  [1, 2, 3],          // i = 0   --
+  [0, 1, 2, 3],       // initialize an array o indexs with all the elements. while this array has length, iterate. 
+                      // while iterating, push the current array using the array of index into the final array. 
+                      // if (first element === 1 && last element === array.length) unshift() and i = 0;
+                      // else i++ 
 ]
 */
 
