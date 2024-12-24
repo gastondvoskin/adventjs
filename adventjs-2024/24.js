8a631d4731771206f017b198a1c49dd79209acfd
Gaston Dvoskin <dvoskingaston@gmail.com>
2024-12-24 09:26:50 -0300

    Solved adventjs-2024 24.js with help from Github Copilot

--- adventjs-2024/24.js
+++ adventjs-2024/24.js
@@ -4,9 +4,41 @@
 // }
 
 function isTreesSynchronized(tree1, tree2) {
-  // Code here
-  return [false, '']
+  if (!tree1 && !tree2) return [true, ""]; 
+  if (!tree1 || !tree2) return [false, tree1 ? tree1.value : tree2.value];
+
+  const equalValues = tree1.value === tree2.value; 
+  if (!equalValues) return [false, tree1.value];
+
+  const leftEqualsRight = isTreesSynchronized(tree1.left, tree2.right);  
+  const rightEqualsLeft = isTreesSynchronized(tree1.right, tree2.left); 
+  if (!leftEqualsRight[0] || !rightEqualsLeft[0]) return [false, tree1.value];
+  
+  return [true, tree1.value]; 
 }
+
+
+
+// TESTS 
+console.log(isTreesSynchronized({value: "a"}, {value: "a"}));  
+console.log(isTreesSynchronized({value: "a"}, {value: "b"}));  
+console.log(isTreesSynchronized({value: "a", left: {value: "b"}}, {value: "a", left: {value: "c"}}));  
+
+/* 
+LOGIC
+if (tree1.value !== tree2.value) return [false, tree1.value]; 
+if (!tree.left && !tree.right) return true; 
+
+const leftEqualsRight = isTreesSynchronized(tree1.left, tree2.right);  
+const rightEqualsLeft = isTreesSynchronized(tree1.right, tree2.left); 
+
+return leftEqualsRight && rightEqualsLeft && [true, tree1.value]
+
+*/
+
+// console.log(true && [true, "a"]);
+// console.log([true, "a"] && [true, "b"]);
+// console.log(undefined.value); 
 
 
 /* 
