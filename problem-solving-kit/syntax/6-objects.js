59c7fe07361bca7fa7493d3be36cc105dce19b5b
Gaston Dvoskin <dvoskingaston@gmail.com>
2025-03-10 07:19:55 -0300

    Wrote destructuring syntax

--- problem-solving-kit/syntax/6-objects.js
+++ problem-solving-kit/syntax/6-objects.js
@@ -3,6 +3,8 @@ const user = {
   age: 30,
 }
 
+
+// dot and bracket notations
 // dot notation
 console.log(user.name); 
 user.age = 31; 
@@ -10,7 +12,11 @@ user.age = 31;
 // bracket notation
 // case 1: accessing a value by the name of the key. equivalent to dot notation
 user["age"] = 32; 
-
 // case 2: accesing a property by a variable which its value is equal to the key. 
 const property = "age";
 user[property] = 33;  
+
+
+// destructuring
+const {name, age} = user; 
+console.log(age); 
