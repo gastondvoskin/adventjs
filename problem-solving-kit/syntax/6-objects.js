9106503f5539fe10bb025f35443bd6f0c34e1fb2
Gaston Dvoskin <dvoskingaston@gmail.com>
2025-03-10 07:44:58 -0300

    Wrote entries syntax

--- problem-solving-kit/syntax/6-objects.js
+++ problem-solving-kit/syntax/6-objects.js
@@ -24,3 +24,41 @@ console.log(age);
 
 // spread
 const copyOfUser = {...user}; // creates a shallow copy
+
+
+
+//-------------------------------------------------------
+// iterating an object
+// entries
+console.log("-------------------loops-------------------");
+console.log("-------------------entries-------------------");
+// An object is not an iterable. It must be converted to an array before iterating the keys, the values, or the pairs of key-values. 
+const person = {
+  name: "paul",
+  age: 30,
+}
+// short path with for...of: Recommended
+// Slightly faster than forEach (due to callback function overhead).
+// allows break
+for (const [key, value] of Object.entries(user)) {
+  console.log({key}, {value}); 
+}
+
+// with forEach: less common
+Object.entries(person).forEach((pair) => {
+  const [key, value] = pair; 
+  console.log({key}, {value}); 
+}) 
+
+
+// long path: less common
+const personKeyValues = Object.entries(person); 
+console.log(personKeyValues); // [ [ 'name', 'paul' ], [ 'age', 30 ] ]
+for (let i = 0; i < personKeyValues.length; i++) {
+  const [key, value] = personKeyValues[i];
+  console.log({i}, {key}, {value}); 
+  // { i: 0 } { key: 'name' } { value: 'paul' }
+  // { i: 1 } { key: 'age' } { value: 30 }
+}
+
+
