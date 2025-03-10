b270848e49700b9445e6b80317189a52e14c9151
Gaston Dvoskin <dvoskingaston@gmail.com>
2025-03-10 10:57:34 -0300

    Documented template literals

---
+++ problem-solving-kit/syntax/7-strings.js
@@ -0,0 +1,36 @@
+const phrase = "paul is a user";
+
+// legth
+// console.log(phrase.length); 
+
+
+
+// iterate a string
+// a string is an iterable but it is immutable. To modify a character, first convert the string to an array, then change the character, then convert the array to string. 
+for (let i = 0; i < phrase.length; i++) {
+  const char = phrase[i];
+  // console.log(char); 
+}
+
+// split
+const splitPhrase = phrase.split(""); 
+// console.log(splitPhrase); 
+
+// join
+// convert an array to a string
+const originalPhrase = splitPhrase.join(""); 
+// console.log(originalPhrase)
+
+// combining split - join
+const chars = phrase.split(""); 
+chars.splice(8, 1, "one"); 
+const modifiedPhrase = chars.join(""); 
+console.log({modifiedPhrase});  
+
+
+// template literals 
+// use case: string interpolation
+const name = "paul"; 
+const text = "is a user"; 
+const result = `${name} ${text}!`; 
+console.log({result});
