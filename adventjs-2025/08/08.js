4ab0946600e4c3228eb5db0e463b67a3e12d161a
Gastón Dvoskin <dvoskingaston@gmail.com>
2026-01-15 14:52:24 +0200

    Solve 2025 - 08.js

--- adventjs-2025/08/08.js
+++ adventjs-2025/08/08.js
@@ -3,6 +3,22 @@
  * @returns {string} The first unique letter in the toy
  */
 function findUniqueToy(toy) {
-  // Code here
-  return ''
+  let letters = toy.split("");
+
+  while (letters.length) {
+    const firstLetter = letters[0];
+    const lengthBeforeFiltering = letters.length;
+    letters = letters.filter(
+      (curentLetter) => curentLetter.toLowerCase() !== firstLetter.toLowerCase()
+    );
+    if (lengthBeforeFiltering - 1 === letters.length) {
+      return firstLetter;
+    }
+  }
+  return "";
 }
+
+// console.log(findUniqueToy("Aaa")); // ''
+// console.log(findUniqueToy("abcDEF")); // 'a'
+// console.log(findUniqueToy("AaBbCc")); // ''
+// console.log(findUniqueToy("sTreSS")); // 'T'
